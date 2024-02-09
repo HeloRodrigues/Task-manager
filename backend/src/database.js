9c684dc486e8525839d6f087bae92d2e9ae9fa34
Heloiza <haraurelio@gmail.com>
2024-02-09 14:19:04 +0100

    Create HTTP routes + middleware

--- backend/src/database.js
+++ backend/src/database.js
@@ -14,35 +14,114 @@ const config = {
 
 const pool = new Pool(config);
 
-pool.connect((error, client) => {
-  if (error) throw error;
+export const getTask = async () => {
+  try {
+    return await new Promise((resolve, reject) => {
+      pool.query("SELECT * FROM tasks", (error, res) => {
+        if (error) {
+          reject(error);
+        }
+        if (res && res.rows) {
+          resolve(res.rows);
+        } else {
+          reject(new Error("No elements found."));
+        }
+      });
+    });
+  } catch (error) {
+    console.log("🚀 ~ getData ~ error:", error);
+    throw new Error("Internal server error");
+  }
+};
 
-  const taskName = "Build to-do app with NodeJs + Postgresql";
-  const nameToDelete = "Build task-manager app";
-
-  client.query(
-    `INSERT INTO tasks (task_name, task_status, start_date, deadline) VALUES ('${taskName}', 'in progress', '2024-01-10', '2024-12-10') returning *`,
-    (error, res) => {
-      if (error) {
-        console.log("data not inserted.", error);
-      } else {
-        console.log("data inserted successfully.");
-        console.log(res.rows);
+export const createTask = (body) => {
+  return new Promise((resolve, reject) => {
+    const { task_name, task_status, start_date, deadline } = body;
+    pool.query(
+      "INSERT INTO tasks (task_name, task_status, start_date, deadline) VALUES ($1, $2, $3, $4) RETURNING *",
+      [task_name, task_status, start_date, deadline],
+      (error, res) => {
+        if (error) {
+          reject(error);
+        }
+        if (res && res.rows) {
+          resolve(`Anew task was added: ${JSON.stringify(res.rows[0])}`);
+        } else {
+          reject(new Error("Unable to add task"));
+        }
       }
-      pool.end;
-    }
-  );
+    );
+  });
+};
 
-  client.query(
-    `DELETE FROM tasks WHERE task_name = '${nameToDelete}' RETURNING *`
-  ),
-    (error, res) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Row deleted!");
-        console.log(res.rows);
+export const deleteTask = (task_name) => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "DELETE FROM tasks WHERE name = $1",
+      [task_name],
+      (error, res) => {
+        if (error) {
+          reject(error);
+        }
+        if (res && res.rows) {
+          resolve(`Task with the name ${task_name} deleted`);
+        } else {
+          reject(new Error("Unable to delete task"));
+        }
       }
-      pool.end;
-    };
-});
+    );
+  });
+};
+
+export const updateTask = (task_status, body) => {
+  return new Promise((resolve, reject) => {
+    const { task_name, start_date, deadline } = body;
+    pool.query(
+      "UPDATE tasks SET task_name = $1, task_status = $2, start_date = $3, deadline = $4",
+      [task_name, task_status, start_date, deadline],
+      (error, res) => {
+        if (error) {
+          reject(error);
+        }
+        if (res && res.rows) {
+          resolve(`Task updated: ${JSON.stringify(res.rows[0])}`);
+        } else {
+          reject(new Error("Unable to update task"));
+        }
+      }
+    );
+  });
+};
+
+// pool.connect((error, client) => {
+//   if (error) throw error;
+
+//   const taskName = "Build to-do app with NodeJs + Postgresql";
+//   const nameToDelete = "Build task-manager app";
+
+//   client.query(
+//     `INSERT INTO tasks (task_name, task_status, start_date, deadline) VALUES ('${taskName}', 'in progress', '2024-01-10', '2024-12-10') returning *`,
+//     (error, res) => {
+//       if (error) {
+//         console.log("data not inserted.", error);
+//       } else {
+//         console.log("data inserted successfully.");
+//         console.log(res.rows);
+//       }
+//       pool.end;
+//     }
+//   );
+
+//   client.query(
+//     `DELETE FROM tasks WHERE task_name = '${nameToDelete}' RETURNING *`
+//   ),
+//     (error, res) => {
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         console.log("Row deleted!");
+//         console.log(res.rows);
+//       }
+//       pool.end;
+//     };
+// });
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,61 @@
 import express from "express";
+import { getTask, createTask, deleteTask, updateTask } from "./database.js";
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-origin",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello node");
+  getTask()
+    .then((res) => {
+      res.status(200).send(res);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  createTask(req.body)
+    .then((res) => {
+      res.status(200).send(res);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/tasks/:task_name", (req, res) => {
+  deleteTask(req.params.task_name)
+    .then((res) => {
+      res.status(200).send(res);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.put("/tasks/:task_status", (req, res) => {
+  const status = req.params.task_status;
+  const body = req.body;
+  updateTask(status, body)
+    .then((res) => {
+      res.status(200).send(res);
+    })
+    .catch(res.status(500).send(error));
 });
 
 app.listen(PORT, () => {
