46d970a2d34b6b954aaa9a1bae6c27089f969501
Heloiza <haraurelio@gmail.com>
2024-02-09 19:11:36 +0100

    Fix error in app.get

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { response } from "express";
 import { getTask, createTask, deleteTask, updateTask } from "./database.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
@@ -20,42 +21,42 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
   getTask()
-    .then((res) => {
-      res.status(200).send(res);
+    .then((response) => {
+      res.status(200).send(response);
     })
     .catch((error) => {
-      res.status(500).send(error);
+      response.status(500).send(error);
     });
 });
 
 app.post("/tasks", (req, res) => {
   createTask(req.body)
-    .then((res) => {
-      res.status(200).send(res);
+    .then((response) => {
+      res.status(200).send(response);
     })
     .catch((error) => {
-      res.status(500).send(error);
+      response.status(500).send(error);
     });
 });
 
-app.delete("/tasks/:task_name", (req, res) => {
-  deleteTask(req.params.task_name)
-    .then((res) => {
-      res.status(200).send(res);
+app.delete("/tasks/:id", (req, res) => {
+  deleteTask(req.params.id)
+    .then((response) => {
+      res.status(200).send(response);
     })
     .catch((error) => {
-      res.status(500).send(error);
+      response.status(500).send(error);
     });
 });
 
-app.put("/tasks/:task_status", (req, res) => {
-  const status = req.params.task_status;
+app.put("/tasks/:id", (req, res) => {
+  const status = req.params.id;
   const body = req.body;
   updateTask(status, body)
-    .then((res) => {
-      res.status(200).send(res);
+    .then((response) => {
+      res.status(200).send(response);
     })
-    .catch(res.status(500).send(error));
+    .catch(response.status(500).send(error));
 });
 
 app.listen(PORT, () => {
